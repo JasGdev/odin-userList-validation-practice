a0fc54ca9d31f82f4e91c30e7088d30cf8528145
Jason <jason13.deng@gmail.com>
2026-03-08 12:02:50 +0900

    Add new  validaters to validateUser

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,22 +2,22 @@
 const usersStorage = require("../storages/usersStorage");
 
 exports.usersListGet = (req, res) => {
-  res.render("index", {
-    title: "User list",
-    users: usersStorage.getUsers(),
-  });
+	res.render("index", {
+		title: "User list",
+		users: usersStorage.getUsers(),
+	});
 };
 
 exports.usersCreateGet = (req, res) => {
-  res.render("createUser", {
-    title: "Create user",
-  });
+	res.render("createUser", {
+		title: "Create user",
+	});
 };
 
 exports.usersCreatePost = (req, res) => {
-  const { firstName, lastName, email, age, bio } = req.body;
-  usersStorage.addUser({ firstName, lastName, email, age, bio});
-  res.redirect("/");
+	const { firstName, lastName, email, age, bio } = req.body;
+	usersStorage.addUser({ firstName, lastName, email, age, bio });
+	res.redirect("/");
 };
 
 // This just shows the new stuff we're adding to the existing contents
@@ -25,62 +25,78 @@ const { body, validationResult, matchedData } = require("express-validator");
 
 const alphaErr = "must only contain letters.";
 const lengthErr = "must be between 1 and 10 characters.";
+const ageErr = "must be an integer between 18 and 120";
 
 const validateUser = [
-  body("firstName").trim()
-    .isAlpha().withMessage(`First name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
-  body("lastName").trim()
-    .isAlpha().withMessage(`Last name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+	body("firstName")
+		.trim()
+		.isAlpha()
+		.withMessage(`First name ${alphaErr}`)
+		.isLength({ min: 1, max: 10 })
+		.withMessage(`First name ${lengthErr}`),
+	body("lastName")
+		.trim()
+		.isAlpha()
+		.withMessage(`Last name ${alphaErr}`)
+		.isLength({ min: 1, max: 10 })
+		.withMessage(`Last name ${lengthErr}`),
+	body("email").trim().isEmail().withMessage(`Please enter a valid email`),
+
+	body("age")
+		.optional({ values: "falsy" })
+		.trim()
+		.isInt({ min: 18, max: 120 })
+		.withMessage(`Age ${ageErr}`),
+	body("bio")
+		.optional({ values: "falsy" })
+		.isLength({ max: 200 })
+		.withMessage(`bio must be under 200 characters`),
 ];
 
 // We can pass an entire array of middleware validations to our controller.
 exports.usersCreatePost = [
-  validateUser,
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).render("createUser", {
-        title: "Create user",
-        errors: errors.array(),
-      });
-    }
-    const { firstName, lastName } = matchedData(req);
-    usersStorage.addUser({ firstName, lastName });
-    res.redirect("/");
-  }
+	validateUser,
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).render("createUser", {
+				title: "Create user",
+				errors: errors.array(),
+			});
+		}
+		const { firstName, lastName } = matchedData(req);
+		usersStorage.addUser({ firstName, lastName });
+		res.redirect("/");
+	},
 ];
 
-
 exports.usersUpdateGet = (req, res) => {
-  const user = usersStorage.getUser(req.params.id);
-  res.render("updateUser", {
-    title: "Update user",
-    user: user,
-  });
+	const user = usersStorage.getUser(req.params.id);
+	res.render("updateUser", {
+		title: "Update user",
+		user: user,
+	});
 };
 
 exports.usersUpdatePost = [
-  validateUser,
-  (req, res) => {
-    const user = usersStorage.getUser(req.params.id);
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).render("updateUser", {
-        title: "Update user",
-        user: user,
-        errors: errors.array(),
-      });
-    }
-    const { firstName, lastName } = matchedData(req);
-    usersStorage.updateUser(req.params.id, { firstName, lastName });
-    res.redirect("/");
-  }
+	validateUser,
+	(req, res) => {
+		const user = usersStorage.getUser(req.params.id);
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).render("updateUser", {
+				title: "Update user",
+				user: user,
+				errors: errors.array(),
+			});
+		}
+		const { firstName, lastName } = matchedData(req);
+		usersStorage.updateUser(req.params.id, { firstName, lastName });
+		res.redirect("/");
+	},
 ];
 
 exports.usersDeletePost = (req, res) => {
-  usersStorage.deleteUser(req.params.id);
-  res.redirect("/");
+	usersStorage.deleteUser(req.params.id);
+	res.redirect("/");
 };
-
